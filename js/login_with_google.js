b88f5cca607e3bef1e220970d52a7e9324738564
PiyushKhandelia <piyushkhandelia2002@gmail.com>
2024-02-28 14:49:38 +0530

    Adding Backend and Database

---
+++ js/login_with_google.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBLHdQpcdEPBxHG8_pI49ctq7FfEDK9KmM",
+    authDomain: "guesthousereservationsystem.firebaseapp.com",
+    projectId: "guesthousereservationsystem",
+    storageBucket: "guesthousereservationsystem.appspot.com",
+    messagingSenderId: "324476262968",
+    appId: "1:324476262968:web:887539776c70257702e4cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en'
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("google-login-btns");
+googleLogin.addEventListener("click", function() {
+
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log(user);
+        alert("Login Successful");
+        window.location.href="https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/booking.html";
+
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+
+} )
