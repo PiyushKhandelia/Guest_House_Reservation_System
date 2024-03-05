521f75686804cad074b6d28d533ccf1ee11db728
PiyushKhandelia <piyushkhandelia2002@gmail.com>
2024-03-05 17:01:21 +0530

    FInal Stag Changes

--- js/login_using_google.js
+++ js/login_using_google.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,23 +18,17 @@ const auth = getAuth(app);
 auth.languageCode = 'en'
 const provider = new GoogleAuthProvider();
 
-const Login = document.getElementById("login-btn");
-Login.addEventListener("click", function (event) {
-    event.preventDefault()
-    
-    //inputs
-    const email = document.getElementById('loginemail').value;
-    const password = document.getElementById('loginPassword').value;
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up 
-            const email = userCredential.email;
-            alert("LogIn Successful");
+const googleLogin = document.getElementById("google-login-btn");
+googleLogin.addEventListener("click", function() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const user = result.user;
+            alert("Login Successful");
             window.location.href="https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/booking.html";
-        })
-        .catch((error) => {
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
             const errorMessage = error.message;
-            alert(errorMessage);
-        });
+    });
 })
