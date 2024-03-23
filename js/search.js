ab5b7a6b1a8d0ca72b8fb0dc9fb3f5f24b0f3d16
PiyushKhandelia <piyushkhandelia2002@gmail.com>
2024-03-24 02:54:39 +0530

    Adding Changes

--- js/search.js
+++ js/search.js
@@ -1,10 +1,4 @@
-// Define showUpdateForm function
-function showUpdateForm() {
-    document.getElementById('my_search_form').style.display = 'none';
-    document.getElementById('updateForm').style.display = 'block';
-}
-
-// Firebase configuration
+// Initialize Firebase first
 var firebaseConfig = {
     apiKey: "AIzaSyBRI8aRPiHrQL9fWgNH9JgWpBtD8GTPuv8",
     authDomain: "guesthousebookingsystem-675c6.firebaseapp.com",
@@ -15,143 +9,155 @@ var firebaseConfig = {
     appId: "1:979837493640:web:64745b6eefd825d4c31a3d"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Check if Firebase app is already initialized
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-// Reference to your Firebase database
-var database = firebase.database();
+// Wrap Script 2 in an IIFE
+(function () {
+    // Define showUpdateForm function
+    function showUpdateForm() {
+        document.getElementById('my_search_form').style.display = 'none';
+        document.getElementById('updateForm').style.display = 'block';
+    }
 
-// Function to search for data based on field type and field value
-function searchByField(fieldType, fieldValue, statusFilter) {
-    var tableBody = document.querySelector("#searchResults tbody");
+    // Reference to your Firebase database
+    var database = firebase.database();
 
-    // Clear existing rows
-    tableBody.innerHTML = "";
+    // Function to search for data based on field type and field value
+    function searchByField(fieldType, fieldValue, statusFilter) {
+        var tableBody = document.querySelector("#searchResults tbody");
 
-    // Reference to the "bookings" node in your database
-    var bookingsRef = firebase.database().ref('bookings');
+        // Clear existing rows
+        tableBody.innerHTML = "";
 
-    // Query the database based on the selected field type and field value
-    if (fieldType === "phonenumber") {
-        // Search by phone number
-        bookingsRef.orderByChild('phone').equalTo(fieldValue).once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                displayBookingData(childSnapshot, tableBody, statusFilter);
+        // Reference to the "bookings" node in your database
+        var bookingsRef = firebase.database().ref('bookings');
+
+        // Query the database based on the selected field type and field value
+        if (fieldType === "phonenumber") {
+            // Search by phone number
+            bookingsRef.orderByChild('phone').equalTo(fieldValue).once('value', function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    displayBookingData(childSnapshot, tableBody, statusFilter);
+                });
             });
-        });
-    } else if (fieldType === "bookingID") {
-        // Search by booking ID
-        bookingsRef.child(fieldValue).once('value', function(snapshot) {
-            if (snapshot.exists()) {
-                displayBookingData(snapshot, tableBody, statusFilter);
+        } else if (fieldType === "bookingID") {
+            // Search by booking ID
+            bookingsRef.child(fieldValue).once('value', function (snapshot) {
+                if (snapshot.exists()) {
+                    displayBookingData(snapshot, tableBody, statusFilter);
+                } else {
+                    alert("Booking not found.");
+                }
+            });
+        }
+    }
+
+    // Function to display booking data in the table
+    function displayBookingData(snapshot, tableBody, statusFilter) {
+        var bookingData = snapshot.val();
+
+        // Check if status filter is applied and if it matches the booking status
+        if (!statusFilter || bookingData.status === statusFilter) {
+            var row = document.createElement("tr");
+
+            // Create cells for each column
+            var bookingIDCell = document.createElement("td");
+            bookingIDCell.textContent = bookingData.bookingID;
+            row.appendChild(bookingIDCell);
+
+            var nameCell = document.createElement("td");
+            nameCell.textContent = bookingData.name;
+            row.appendChild(nameCell);
+
+            var emailCell = document.createElement("td");
+            emailCell.textContent = bookingData.email;
+            row.appendChild(emailCell);
+
+            var phoneCell = document.createElement("td");
+            phoneCell.textContent = bookingData.phone;
+            row.appendChild(phoneCell);
+
+            var guestHouseCell = document.createElement("td");
+            guestHouseCell.textContent = bookingData.guestHouse;
+            row.appendChild(guestHouseCell);
+
+            var roomTypeCell = document.createElement("td");
+            roomTypeCell.textContent = bookingData.roomType;
+            row.appendChild(roomTypeCell);
+
+            var checkInCell = document.createElement("td");
+            checkInCell.textContent = bookingData.dateF;
+            row.appendChild(checkInCell);
+
+            var checkOutCell = document.createElement("td");
+            checkOutCell.textContent = bookingData.dateT;
+            row.appendChild(checkOutCell);
+
+            var statusCell = document.createElement("td");
+            statusCell.textContent = bookingData.status;
+            row.appendChild(statusCell);
+
+            // Create action buttons
+            var updateButton = document.createElement("button");
+            updateButton.textContent = "Update";
+            updateButton.onclick = function () {
+                showUpdateForm(); // Passing the booking ID to updateBooking
+            };
+
+            var cancelButton = document.createElement("button");
+            cancelButton.textContent = "Cancel";
+            cancelButton.onclick = function () {
+                cancelBooking(snapshot.key); // Passing the booking ID to cancelBooking
+            };
+
+            // Create a cell to hold the action buttons
+            var actionCell = document.createElement("td");
+            actionCell.appendChild(updateButton);
+            actionCell.appendChild(cancelButton);
+
+            row.appendChild(actionCell);
+
+            // Append row to the table body
+            tableBody.appendChild(row);
+        }
+    }
+
+    // Function to cancel a booking
+    function cancelBooking(bookingID) {
+        var bookingRef = firebase.database().ref('bookings/' + bookingID);
+        bookingRef.once('value', function (snapshot) {
+            var bookingData = snapshot.val();
+            if (bookingData.status === "Upcoming") {
+                bookingRef.update({ status: "Cancel" })
+                    .then(function () {
+                        alert("Booking canceled successfully.");
+                    })
+                    .catch(function (error) {
+                        alert("Error canceling booking:", error);
+                    });
             } else {
-                alert("Booking not found.");
+                alert("Booking cannot be canceled.");
             }
         });
     }
-}
 
-// Function to display booking data in the table
-function displayBookingData(snapshot, tableBody, statusFilter) {
-    var bookingData = snapshot.val();
+    // Function to handle form submission
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.getElementById('searchform');
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
 
-    // Check if status filter is applied and if it matches the booking status
-    if (!statusFilter || bookingData.status === statusFilter) {
-        var row = document.createElement("tr");
+            // Get the search type and field value from the form
+            var searchType = document.getElementById("searchType").value;
+            var searchField = document.getElementById("searchField").value;
+            var statusFilter = document.getElementById("statusFilter").value;
 
-        // Create cells for each column
-        var bookingIDCell = document.createElement("td");
-        bookingIDCell.textContent = bookingData.bookingID;
-        row.appendChild(bookingIDCell);
-
-        var nameCell = document.createElement("td");
-        nameCell.textContent = bookingData.name;
-        row.appendChild(nameCell);
-
-        var emailCell = document.createElement("td");
-        emailCell.textContent = bookingData.email;
-        row.appendChild(emailCell);
-
-        var phoneCell = document.createElement("td");
-        phoneCell.textContent = bookingData.phone;
-        row.appendChild(phoneCell);
-
-        var guestHouseCell = document.createElement("td");
-        guestHouseCell.textContent = bookingData.guestHouse;
-        row.appendChild(guestHouseCell);
-        
-        var roomTypeCell = document.createElement("td");
-        roomTypeCell.textContent = bookingData.roomType;
-        row.appendChild(roomTypeCell);
-
-        var checkInCell = document.createElement("td");
-        checkInCell.textContent = bookingData.dateF;
-        row.appendChild(checkInCell);
-
-        var checkOutCell = document.createElement("td");
-        checkOutCell.textContent = bookingData.dateT;
-        row.appendChild(checkOutCell);
-
-        var statusCell = document.createElement("td");
-        statusCell.textContent = bookingData.status;
-        row.appendChild(statusCell);
-
-        // Create action buttons
-        var updateButton = document.createElement("button");
-        updateButton.textContent = "Update";
-        updateButton.onclick = function() {
-            showUpdateForm(); // Passing the booking ID to updateBooking
-        };
-
-        var cancelButton = document.createElement("button");
-        cancelButton.textContent = "Cancel";
-        cancelButton.onclick = function() {
-            cancelBooking(snapshot.key); // Passing the booking ID to cancelBooking
-        };
-
-        // Create a cell to hold the action buttons
-        var actionCell = document.createElement("td");
-        actionCell.appendChild(updateButton);
-        actionCell.appendChild(cancelButton);
-
-        row.appendChild(actionCell);
-
-        // Append row to the table body
-        tableBody.appendChild(row);
-    }
-}
-
-// Function to cancel a booking
-function cancelBooking(bookingID) {
-    var bookingRef = firebase.database().ref('bookings/' + bookingID);
-    bookingRef.once('value', function(snapshot) {
-        var bookingData = snapshot.val();
-        if (bookingData.status === "Upcoming") {
-            bookingRef.update({ status: "Cancel" })
-            .then(function() {
-                alert("Booking canceled successfully.");
-            })
-            .catch(function(error) {
-                alert("Error canceling booking:", error);
-            });
-        } else {
-            alert("Booking cannot be canceled.");
-        }
+            // Call the search function with the search type, field value, and status filter
+            searchByField(searchType, searchField, statusFilter);
+        });
     });
-}
 
-// Function to handle form submission
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('searchform');
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        // Get the search type and field value from the form
-        var searchType = document.getElementById("searchType").value;
-        var searchField = document.getElementById("searchField").value;
-        var statusFilter = document.getElementById("statusFilter").value;
-
-        // Call the search function with the search type, field value, and status filter
-        searchByField(searchType, searchField, statusFilter);
-    });
-});
+})();
--- js/user_modify.js
+++ js/user_modify.js
@@ -1,87 +1,148 @@
+(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBRI8aRPiHrQL9fWgNH9JgWpBtD8GTPuv8",
+        authDomain: "guesthousebookingsystem-675c6.firebaseapp.com",
+        databaseURL: "https://guesthousebookingsystem-675c6-default-rtdb.firebaseio.com",
+        projectId: "guesthousebookingsystem-675c6",
+        storageBucket: "guesthousebookingsystem-675c6.appspot.com",
+        messagingSenderId: "979837493640",
+        appId: "1:979837493640:web:64745b6eefd825d4c31a3d"
+    };
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBRI8aRPiHrQL9fWgNH9JgWpBtD8GTPuv8",
-    authDomain: "guesthousebookingsystem-675c6.firebaseapp.com",
-    databaseURL: "https://guesthousebookingsystem-675c6-default-rtdb.firebaseio.com",
-    projectId: "guesthousebookingsystem-675c6",
-    storageBucket: "guesthousebookingsystem-675c6.appspot.com",
-    messagingSenderId: "979837493640",
-    appId: "1:979837493640:web:64745b6eefd825d4c31a3d"
-};
+    firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+    const database = firebase.database();
+    const reference = firebase.database().ref("bookings");
+    let updateOption;
 
-// Get a reference to the database service
-const database = firebase.database();
-const ref = database.ref("bookings");
+    function updateRoomOptions() {
+        // Function to update room options based on selected guest house
+        const guestHouse = document.getElementById('guest_house').value;
+        const roomTypeSelect = document.getElementById('room_type');
+        roomTypeSelect.innerHTML = ''; // Clear existing options
 
-// Function to handle form submission for update
-document.addEventListener('DOMContentLoaded', function () {
-    const updateForm = document.getElementById('updateform');
-    updateForm.addEventListener('submit', function (e) {
-        e.preventDefault();
+        if (guestHouse === 'igh') {
+            // Add options for IGH Guest House
+            const options = [
+                { value: 'single', text: 'Single' },
+                { value: 'mezzanine', text: 'Mezzanine' },
+                { value: 'suite', text: 'Suite' }
+            ];
 
-        // Get the selected option for update
-        var updateOption = document.getElementById("change").value;
+            options.forEach(option => {
+                const optionElement = document.createElement('option');
+                optionElement.value = option.value;
+                optionElement.textContent = option.text;
+                roomTypeSelect.appendChild(optionElement);
+            });
+        } else if (guestHouse === 'ogh') {
+            // Add options for OGH Guest House
+            const options = [
+                { value: 'single', text: 'Single' },
+                { value: 'suite', text: 'Suite' }
+            ];
 
-        // Perform update operation based on selected option
-        if (updateOption === "DateF&T") {
-            var dateFrom = document.getElementById("fromDate").value;
-            var dateTo = document.getElementById("toDate").value;
+            options.forEach(option => {
+                const optionElement = document.createElement('option');
+                optionElement.value = option.value;
+                optionElement.textContent = option.text;
+                roomTypeSelect.appendChild(optionElement);
+            });
+        }
+    }
 
-            // Push data to Firebase
-            ref.push({
-                dateFrom: dateFrom,
-                dateTo: dateTo
-                // Add other form fields here
-            }, function (error) {
-                if (error) {
-                    alert("Error occurred while saving data: " + error.message);
+    document.getElementById('guest_house').addEventListener('change', updateRoomOptions);
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const updateForm = document.getElementById('updateform');
+        updateForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const bookingID = document.getElementById('booking_id').value.trim();
+            const bookingRef = reference.child(bookingID);
+            const updateOption = document.getElementById("change").value;
+
+            if (updateOption === "DateF&T") {
+                // Update date and time
+                const dateFrom = new Date(document.getElementById('fromDate').value);
+                    const dateTo = new Date(document.getElementById('toDate').value);
+        
+                    bookingRef.update({
+                        dateFrom: dateFrom.toISOString(),
+                        dateTo: dateTo.toISOString()
+                    }).then(function () {
+                        alert("Modification Successful");
+                        window.location.href = "https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
+                    }).catch(function (error) {
+                        console.error("Error updating date:", error);
+                        alert("Error occurred while updating date: " + error.message);
+                    });
+            } else if (updateOption === "guesthouse") {
+                // Update guesthouse and room type
+                const selectedGuesthouse = document.getElementById("guest_house").value.trim();
+                    const selectedRoomType = document.getElementById("room_type").value.trim();
+                    bookingRef.update({
+                        guestHouse: selectedGuesthouse,
+                        roomType: selectedRoomType
+                    }).then(function () {
+                        alert("Modification Successful");
+                        window.location.href = "https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
+                    }).catch(function (error) {
+                        console.error("Error updating guesthouse:", error);
+                        alert("Error occurred while updating guesthouse: " + error.message);
+                    });
+            } else if (updateOption === "Occupacy") {
+                // Update occupancy
+                const selectedOccupancy = document.getElementById("occupancy").value.trim();
+                    bookingRef.update({
+                        occupancy: selectedOccupancy
+                    }).then(function () {
+                        alert("Modification Successful");
+                        window.location.href = "https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
+                    }).catch(function (error) {
+                        console.error("Error updating occupancy:", error);
+                        alert("Error occurred while updating occupancy: " + error.message);
+                    });
+            }
+        });
+    });
+
+    document.getElementById('cancelButton').addEventListener('click', function () {
+        const bookingIDElement = document.getElementById('booking_id');
+        if (bookingIDElement) {
+            const bookingID = bookingIDElement.value.trim();
+            const bookingRef = reference.child(bookingID);
+            const updateOption = document.getElementById("change").value;
+
+            bookingRef.once('value', function (snapshot) {
+                const bookingData = snapshot.val();
+                if (bookingData.status === "Upcoming") {
+                    bookingRef.update({ status: "Cancelled" })
+                    .then(function () {
+                        alert("Booking canceled successfully.");
+                    })
+                    .catch(function (error) {
+                        alert("Error canceling booking: " + error);
+                    });
                 } else {
-                    alert("Modification Succesfull");
-                    window.location.href="https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
+                    alert("Booking cannot be canceled.");
                 }
             });
-        } else if (updateOption === "guesthouse") {
-            var selectedGuesthouse = document.getElementById("guesthouse").value;
-            var selectedRoomType = document.getElementById("roomType").value;
-
-            // Push data to Firebase
-            ref.push({
-                guesthouse: selectedGuesthouse,
-                roomType: selectedRoomType
-                // Add other form fields here
-            }, function (error) {
-                if (error) {
-                    alert("Error occurred while saving data: " + error.message);
-                } else {
-                    alert("Modification Succesfull");
-                    window.location.href="https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
-                }
-            });
-        } else if (updateOption === "Occupacy") {
-            var selectedOccupancy = document.getElementById("occupancy").value;
-
-            // Push data to Firebase
-            ref.push({
-                occupacy: selectedOccupancy
-                // Add other form fields here
-            }, function (error) {
-                if (error) {
-                    alert("Error occurred while saving data: " + error.message);
-                } else {
-                    alert("Modification Succesfull");
-                    window.location.href="https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/my_bookings.html";
-                }
-            });
+        } else {
+            console.error("Element with ID 'booking_id' not found.");
         }
     });
 
-    // Function to handle cancel button click
-    const cancelButton = document.getElementById('cancelButton');
-    cancelButton.addEventListener('click', function () {
-        // Perform session timeout
-        // Redirect user to login/signup page
-        window.location.href = "https://piyushkhandelia.github.io/Guest_House_Reservation_System/Pages/login_signup.html";
+    document.getElementById('proceedButton').addEventListener('click', function () {
+        proceedWithUpdate();
     });
-});
+
+    function proceedWithUpdate() {
+        updateOption = document.getElementById("change").value;
+
+        // Show/hide fields based on selected option
+        document.getElementById('guesthouseFields').style.display = updateOption === 'guesthouse' ? 'block' : 'none';
+        document.getElementById('dateFields').style.display = updateOption === 'DateF&T' ? 'block' : 'none';
+        document.getElementById('occupancyFields').style.display = updateOption === 'Occupacy' ? 'block' : 'none';
+    }
+})();
